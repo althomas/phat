f2542da7223b2440772d17e02751fffabd62dd92
Ashleigh <althomas41@gmail.com>
2013-03-11 16:43:31 -0400

    output now includes quivers (arrows) for velocity data and 'x's for vision measurements
    implemented config files through sim link from a file in config folder
    to ekf/config.m.

--- ekf/read_logs.m
+++ ekf/read_logs.m
@@ -1,13 +1,10 @@
 % reads log files 
 
 %function [est, truth]=read_logs(logfile, test)
-function [in,vis]=read_logs
+function [in,vis]=read_logs(logfile_in, logfile_vis)
 
-%logfile = './logs/log_1.txt';
-logfile_in = './logs/test_in.txt';
-logfile_vis = './logs/test_vis.txt';
-%logfile = './logs/restData.txt';
 
-%tdfread(logfile);
 in = dlmread(logfile_in, '\t'); 
 vis = dlmread(logfile_vis, '\t'); 
+
+end
--- ekf/run_ekf.m
+++ ekf/run_ekf.m
@@ -3,46 +3,44 @@
 % starting point to run ekf
 
 clear all;
+clf;
 
-n=3;      %number of states (make sure to change f and s accordingly)
-q=0.01;    %std of process 
-r=0.01;    %std of measurement 
-Q=q^2*eye(n); % covariance of process
-R=r^2*eye(n);        % covariance of measurement
-t = .01;        %timestep in seconds
-f=@(x,u,t)[x(1)+t*u(1);x(2)+t*u(2);x(3)+t*u(3)];  % nonlinear state equations
-h=@(x,u,t)[x(1);x(2);x(3)];                               % measurement equation
-x_0=[0;0;0];                              % initial state (note: this used to be called s)
-x=x_0+q*randn(n,1); %initial state          % initial state with noise
-P = eye(n);                               % initial state covraiance
-N=20;                                     % total dynamic steps
+c = config();
+
+n=c.n;  
+q=c.q;   
+r=c.r;   
+Q=c.Q; 
+R=c.R; 
+t=c.t;
+f=c.f;  
+h=c.h;                              
+x_0=c.x_0;                              
+x=x_0+q*randn(n,1); 
+P=c.P;          
+N=c.N;                
 xV = zeros(n,N);          %estimate       % allocate memory
 sV = zeros(n,N);          %actual
-zV = zeros(1,N); % do we need this?
+%zV = zeros(1,N); % do we need this?
 
 
-% create test data and update process
-%for k=1:N
-%  z = h(s) + r*randn;                     % measurments (create test data)
-%  sV(:,k)= s;                             % save actual state
-%  zV(k)  = z;                             % save measurment
-%  [x, P] = ekf(f,x,P,h,z,Q,R);            % ekf
-%  xV(:,k) = x;                            % save estimate
-%  s = f(s) + q*randn(n,1);                % update process
-%end
 
 % read in test data (with truth) 
-[in,vis]=read_logs();
+[in,vis]=read_logs(c.testlog_in,c.testlog_vis);
 % at this point we have to match up timestamps (and length!), but for now we assume
 % they match up perfectly
 
 data = cat(2,in(:,2:4),vis(:,2:end)); % this throws out the timestamp entirely
-N = length(vis(:,1));
+N = length(vis(:,1)); % Should replace this with "read until there's no more to read"
+
+
 
 
 for k=1:N
   z = vis(k,2:end)' + r*randn(3,1);                    % actual measurments from vision
+  zV(:,k) = z;                                  % save vision measurements for plots
   u = in(k,2:4)' + q*randn(3,1);                      % inertial measurements
+  uV(:,k) = u;
   [x, P] = ekf(f,x,u,t,P,h,z,Q,R);            % ekf
   xV(:,k) = x;                            % save estimate
 %  s = f(s) + q*randn(n,1);                % update process
@@ -54,42 +52,20 @@ end
 %for k=1:n                                 % plot results
 %  subplot(n,1,k)
   %plot(1:N, sV(k,:), '-', 1:N, xV(k,:), '--')
-  plot(1:N, vis(:,2),'b', 1:N, xV(1,:), 'r:o');
+  hold on;
+  plot( 1:N, vis(:,2),'b',1:N, xV(1,:), 'r:o'); %ekf estimate coordinates
+  scatter(1:N, zV(1,:), 'kx'); % vision measured coordinates  
+
+  quiver(1:N, xV(1,:), cos(uV(1,:)), sin(uV(1,:)), .1 );
+ 
+
   xlabel('timestep');
   ylabel('x position');
-  legend('true position','estimate from EKF');
+  legend('true position','estimate from EKF', 'measured position (from vision)');
   title('Estimate using noisy data vs true readings of x position. Std of measurements: 0.01 ');
   axis([0 20 -.2 1.4]);
   %legend ('actual state', 'estimate', 'Location', 'EastOutside');
   %legend ('estimate', 'Location', 'EastOutside');
 %end
 
-% Original example
-%{
-n=3;      %number of state
-q=0.1;    %std of process 
-r=0.1;    %std of measurement
-Q=q^2*eye(n); % covariance of process
-R=r^2;        % covariance of measurement  
-f=@(x)[x(2);x(3);0.05*x(1)*(x(2)+x(3))];  % nonlinear state equations
-h=@(x)x(1);                               % measurement equation
-s=[0;0;1];                                % initial state
-x=s+q*randn(3,1); %initial state          % initial state with noise
-P = eye(n);                               % initial state covraiance
-N=20;                                     % total dynamic steps
-xV = zeros(n,N);          %estmate        % allocate memory
-sV = zeros(n,N);          %actual
-zV = zeros(1,N);
-for k=1:N
-  z = h(s) + r*randn;                     % measurments
-  sV(:,k)= s;                             % save actual state
-  zV(k)  = z;                             % save measurment
-  [x, P] = ekf(f,x,P,h,z,Q,R);            % ekf 
-  xV(:,k) = x;                            % save estimate
-  s = f(s) + q*randn(3,1);                % update process 
-end
-for k=1:3                                 % plot results
-  subplot(3,1,k)
-  plot(1:N, sV(k,:), '-', 1:N, xV(k,:), '--')
-end
-%}
+
