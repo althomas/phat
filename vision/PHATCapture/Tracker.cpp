0004369d7ae4c2de067fb5ff80ab78b332a82166
ccog <cogan.cam@gmail.com>
2013-04-08 18:58:13 -0300

    Cleaned up Tracker.cpp
    Tracker.cpp now features more responsible memory management in case of
    errors during runtime and now has up-to-date comments.

--- vision/PHATCapture/Tracker.cpp
+++ vision/PHATCapture/Tracker.cpp
@@ -3,9 +3,9 @@
    Desc: Implementation file for video analysis class
    Credits: The implementation below would not have been possible without the
             assistance of the video "OpenCV tutorial 1 - config with MS VC++
-	    2010, 1st object recognition and tracking program" (available at
-	    www.youtube.com/watch?v=2i2bt-YSIYQ as of 4/8/2013) by the Youtube
-	    user 18F4550videos.
+		    2010, 1st object recognition and tracking program" (available at
+		    www.youtube.com/watch?v=2i2bt-YSIYQ as of 4/8/2013) by the Youtube
+		    user 18F4550videos.
 */
 
 #include "Tracker.h"
@@ -49,6 +49,7 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 
 	if((inputStringLength + folderStringLength + 1) > BUFFER_SIZE){
 		fprintf(stderr,"ERROR: File output name larger than max buffer size of %d.\n",BUFFER_SIZE);
+		free(outputFileName);
 		return -1;
 	}
 
@@ -73,16 +74,19 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 	//recovered.
 	if(!out && strcmp(this->textFilePath,"")){
 		fprintf(stderr,"ERROR: failed to open output text file: \n%s\n\n",outputFileName);
+		free(outputFileName);
 		return DIRECTORY_NAME_FAILURE;
 	}
 
 	if(!(out || strcmp(this->textFilePath,""))){
 		fprintf(stderr,"ERROR: failed to open output text file: \n%s\n\n",outputFileName);
+		free(outputFileName);
 		return EMPTY_DIRECTORY_NAME_FAILURE;
 	}
 
 	if(!out){
 		fprintf(stderr,"ERROR: failed to open output text file: \n%s\n\n",outputFileName);
+		free(outputFileName);
 		return -1;
 	}
 
@@ -93,56 +97,92 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 
 	if(!p_capVideo){
 		fprintf(stderr,"Failure initializing video stream for file %s\n\n",videoFileName);
+		fclose(out);
+		free(outputFileName);
 		return -1;
 	}
 
-	/*Apparently IPL is short for Intel Image Processing Library, and is the standard struct used n OpenCV1.x */
-	IplImage *p_imgOriginal = NULL;				//Pointer to an image structure; this will be the input image from the webcam
-	IplImage *p_imgProcessedRed = NULL;			//Pointer to an image structure; this will be the processed image
-	IplImage *p_imgProcessedBlu = NULL;			//Pointer to an image structure; this will be the processed image
-	IplImage *p_imgProcessedGrn = NULL;			//Pointer to an image structure; this will be the processed image
-	IplImage *p_imgProcessedYlo = NULL;			//Pointer to an image structure; this will be the processed image
-	IplImage *p_imgProcessed = NULL;			//Pointer to an image structure; this will be the processed image
+	//A set of IPL Images pointers (the standard image struct for OpenCV in C)
+	//for frames pre- and post-analysis.
+	IplImage *p_imgOriginal = NULL;		//The original		
+	IplImage *p_imgProcessedRed = NULL;	//Filtered for red		
+	IplImage *p_imgProcessedBlu = NULL;	//Filtered for blue
+	IplImage *p_imgProcessedGrn = NULL;	//Filtered for green
+	IplImage *p_imgProcessedYlo = NULL;	//Filtered for yellow
+	IplImage *p_imgProcessed = NULL;	//The four above images combined for viewing
 
-	//An array to hold the processed images for each color, allowing us to find the beacons color-by-color. Assumes RGBY beacons.
+	//An array to hold the processed images for each color, allowing us to
+	//find the beacons color-by-color. Assumes RGBY beacons.
 	IplImage **processedImages = (IplImage **) malloc(sizeof(IplImage*) * NUM_BEACONS);
 
 	//An array for storing the location data for each beacon at each frame in the video
 	double *data_out = (double *) malloc(sizeof(double) * NUM_BEACONS * 2);
 
+	if(!(processedImages && data_out)){
+		fprintf(stderr,"Problem allocating memory for buffers processedImages and data_out.\n");
+		free(outputFileName);
+		fclose(out);
+		if(processedImages){
+			free(processedImages);
+		}
+		if(data_out){
+			free(data_out);
+		}
+		cvReleaseCapture(&p_capVideo);
+		return -1;
+	}
+
 	for(i = 0; i < NUM_BEACONS * 2; i++){
 		data_out[i] = 0.0;
 	}
 
-	CvMemStorage *p_strStorage = NULL;		//A necessary sorting variable to pass into HoughCircles()
+	//A necessary sorting variable to pass into HoughCircles()
+	CvMemStorage *p_strStorage = NULL;	
 
-	CvSeq *p_seqCircles = NULL;				//Will be returned by cvHoughCircles() and contain all circles
-											//Call cvGetSeqElem(p_seqCircles, i) will return a 3-element array of the ith circle
-	
-	float *p_fltXYRadius = NULL;			//Pointer to the the 3-element array ^
-											//[0] => x position of detected object; [1] => y position of detected object; [2] => radius
+	//Buffer into which cvHoughCircles will write circle information
+	CvSeq *p_seqCircles = NULL;			
+										
+	//Pointer to allow access to the the 3-element arrays contained in
+	//p_seqCircles. 3-element array will by in form [x; y; r]
+	float *p_fltXYRadius = NULL;		
 
-	char charCheckForEscKey;				//char for checking key press (Esc exits program)
+	//char for checking key press to exit program
+	char charCheckForEscKey;
 
 	//Declare analysis windows
-	//cvNamedWindow("Original Image", CV_WINDOW_AUTOSIZE);	//Original image from webcam
+	//cvNamedWindow("Original Image", CV_WINDOW_AUTOSIZE);	//Original image
 	cvNamedWindow("Processed Image", CV_WINDOW_AUTOSIZE);	//Processed image
 
+	//Here we acquire qualities about the video to calibrate the IPLImage buffers
 	p_imgOriginal = cvQueryFrame(p_capVideo);
+
+	//Cancel if frame acquisition failed
+	if(!p_imgOriginal){
+		fprintf(stderr,"ERROR: p_imgOriginal is NULL (capture failed).\n");
+		fclose(out);
+		free(outputFileName);
+		free(processedImages);
+		free(data_out);
+		cvReleaseCapture(&p_capVideo);
+		return -1;
+	}
 
 	int width = p_imgOriginal->width;
 	int height = p_imgOriginal->height;
 	int channels = p_imgOriginal->nChannels;
 	int step = p_imgOriginal->widthStep;
 
+	//Creating a macro for the correct size
 	CvSize p_imgSize = cvSize(width, height);
 	
+	//Now, assign the buffers correct sizes accordingly
 	p_imgProcessed = cvCreateImage(p_imgSize, IPL_DEPTH_8U, 1);
 	p_imgProcessedRed = cvCreateImage(p_imgSize, IPL_DEPTH_8U, 1);
 	p_imgProcessedGrn = cvCreateImage(p_imgSize, IPL_DEPTH_8U, 1);
 	p_imgProcessedBlu = cvCreateImage(p_imgSize, IPL_DEPTH_8U, 1);
 	p_imgProcessedYlo = cvCreateImage(p_imgSize, IPL_DEPTH_8U, 1);
 
+	//Assign values to image buffer to allow for iteration
 	processedImages[0] = p_imgProcessedRed;
 	processedImages[1] = p_imgProcessedGrn;
 	processedImages[2] = p_imgProcessedBlu;
@@ -152,16 +192,10 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 
 	fps = cvGetCaptureProperty(p_capVideo, CV_CAP_PROP_FPS);
 
-	p_imgOriginal = cvQueryFrame(p_capVideo);	//Get frame
-
+	//Deprecated fprintf statement outlining format of output text file
 	//fprintf(out, "Timestamp\tRx\tRy\tGx\tGy\tBx\tBy\tYx\tYy\n");
-
-	if(p_imgOriginal == NULL){
-		printf("ERROR: p_imgOriginal is NULL (capture failed).\n");
-		return -1;
-	}
 	
-	while(1){								//Use ESC key to exit
+	while(1){
 
 		if(!p_imgOriginal){
 			printf("Reached end of video file.\n\n");
@@ -175,48 +209,49 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 				  CV_RGB(255, 120, 120),	//Max filtering value--...and if color is less than this
 				  p_imgProcessedRed);		//Function output (void function, paramter passed by reference)
 
-		cvInRangeS(p_imgOriginal,			//Function input
-				  CV_RGB(0, 170, 0),		//Min filtering value--if color is greater or equal to this...
-				  CV_RGB(200, 255, 160),	//Max filtering value--...and if color is less than this
-				  p_imgProcessedGrn);		//Function output (void function, paramter passed by reference)
+		cvInRangeS(p_imgOriginal,			
+				  CV_RGB(0, 170, 0),		
+				  CV_RGB(200, 255, 160),	
+				  p_imgProcessedGrn);		
 		
-		cvInRangeS(p_imgOriginal,			//Function input
-				  CV_RGB(0, 0, 170),		//Min filtering value--if color is greater or equal to this...
-				  CV_RGB(120, 120, 255),	//Max filtering value--...and if color is less than this
-				  p_imgProcessedBlu);		//Function output (void function, paramter passed by reference)
+		cvInRangeS(p_imgOriginal,			
+				  CV_RGB(0, 0, 170),		
+				  CV_RGB(120, 120, 255),	
+				  p_imgProcessedBlu);		
 
-		cvInRangeS(p_imgOriginal,			//Function input
-				  CV_RGB(210, 180, 40),		//Min filtering value--if color is greater or equal to this...
-				  CV_RGB(255, 255, 150),	//Max filtering value--...and if color is less than this
-				  p_imgProcessedYlo);		//Function output (void function, paramter passed by reference)
+		cvInRangeS(p_imgOriginal,			
+				  CV_RGB(210, 180, 40),		
+				  CV_RGB(255, 255, 150),	
+				  p_imgProcessedYlo);		
 
-		p_strStorage = cvCreateMemStorage(0);	//Allocate necessary memory variable to pass into cvHoughCircles
+		//Allocate necessary memory variable to pass into cvHoughCircles
+		p_strStorage = cvCreateMemStorage(0);
 
 		//Here, we smooth the images for each color, making it easier for Hough Circles to work with
 
 		cvSmooth(p_imgProcessedRed,			//Function input
-				 p_imgProcessedRed,			//Function output (void, pass by reference)
+				 p_imgProcessedRed,			//Function output (void return type, so pass by reference)
 				 CV_GAUSSIAN,				//Use Gaussian filter (averages nearby pixels, with closest weighted more)
 				 9,							//Smoothing window width
 				 9);						//Smoothing window height
 
-		cvSmooth(p_imgProcessedGrn,			//Function input
-				 p_imgProcessedGrn,			//Function output (void, pass by reference)
-				 CV_GAUSSIAN,				//Use Gaussian filter (averages nearby pixels, with closest weighted more)
-				 9,							//Smoothing window width
-				 9);						//Smoothing window height
+		cvSmooth(p_imgProcessedGrn,			
+				 p_imgProcessedGrn,			
+				 CV_GAUSSIAN,			
+				 9,							
+				 9);						
 
-		cvSmooth(p_imgProcessedBlu,			//Function input
-				 p_imgProcessedBlu,			//Function output (void, pass by reference)
-				 CV_GAUSSIAN,				//Use Gaussian filter (averages nearby pixels, with closest weighted more)
-				 9,						//Smoothing window width
-				 9);						//Smoothing window height
+		cvSmooth(p_imgProcessedBlu,			
+				 p_imgProcessedBlu,			
+				 CV_GAUSSIAN,		
+				 9,				
+				 9);			
 
-		cvSmooth(p_imgProcessedYlo,			//Function input
-				 p_imgProcessedYlo,			//Function output (void, pass by reference)
-				 CV_GAUSSIAN,				//Use Gaussian filter (averages nearby pixels, with closest weighted more)
-				 9,							//Smoothing window width
-				 9);						//Smoothing window height
+		cvSmooth(p_imgProcessedYlo,		
+				 p_imgProcessedYlo,	
+				 CV_GAUSSIAN,	
+				 9,				
+				 9);		
 
 		for(i = 0; i < NUM_BEACONS; i++){
 			//Below we grab the data from each color, one at a time, and write the location of the beacons, along with the timestamp
@@ -233,26 +268,34 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 
 			//Extract data from result of Hough Circles algorithm into data_out; use zeros to indicate no beacon found.
 			if(p_seqCircles->total){
-				//Extract information about beacon. NOTE: this assumes that only one beacon per color is found, as it only takes the
-				//first element of p_seqCircles. If more than one "beacon" is found, the real beacon may be neglected.
+				//Extract information about beacon. NOTE: this assumes that only
+				//one beacon per color is found, as it only takes the first
+				//element of p_seqCircles. If more than one "beacon" is found,
+				//the real beacon may be neglected.
 				p_fltXYRadius = (float *)cvGetSeqElem(p_seqCircles, 0);
 				data_out[i * 2] = p_fltXYRadius[0];
 				data_out[i * 2 + 1] = p_fltXYRadius[1];
 			} else {
+				//If no circle is found, indicate as such in buffer with value 0.0
 				data_out[i * 2] = 0.0;
 				data_out[i * 2 + 1] = 0.0;
 			}
-			//printf("Beacon %d position: x = %f, y = %f\n", i, data_out[i * 2], data_out[i * 2 + 1]);
 
 			//Write data to output text file
 			switch (i)
 			{
 			case 0: 
-				//First beacon: include timestamp
+				//First beacon: include timestamp. Note that timestamp values
+				//are calculated via the framerate, necessitating that the .avi
+				//file framerate reflects the real-time capture rate.
 				fprintf(out, "%f,%d,%d,", ((float) frame_count) / fps, (int) data_out[i * 2], (int) data_out[i * 2 + 1]);
 				if(data_out[i * 2] != 0.0){
-					cvCircle(p_imgProcessedRed,				//Draw a red circle around the detected object
-						 cvPoint(cvRound(data_out[i * 2]), cvRound(data_out[i * 2 + 1])),
+					//Here we mark the beacon found in a visible window. This 
+					//is repeated for each beacon and the outputs are combined
+					//using an alpha blend into one picture for viewing.
+					cvCircle(p_imgProcessedRed,		
+						 cvPoint(cvRound(data_out[i * 2]), //Circle x coordinate
+						 cvRound(data_out[i * 2 + 1])),    //Circle y coordinate
 						 10,						//Circle of radius 10
 						 CV_RGB(255, 255, 255),		//White circle
 						 3);						//3 pixels thick
@@ -261,36 +304,44 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 			case 1:
 				fprintf(out, "%d,%d,", (int) data_out[i * 2], (int) data_out[i * 2 + 1]);
 				if(data_out[i * 2] != 0.0){
-					cvCircle(p_imgProcessedGrn,				//Draw a red circle around the detected object
-						 cvPoint(cvRound(data_out[i * 2]), cvRound(data_out[i * 2 + 1])),
-						 10,						//Circle of radius 10
-						 CV_RGB(255, 255, 255),		//White circle
-						 3);						//3 pixels thick
+					cvCircle(p_imgProcessedGrn,	
+						 cvPoint(cvRound(data_out[i * 2]),
+						 cvRound(data_out[i * 2 + 1])),
+						 10,						
+						 CV_RGB(255, 255, 255),		
+						 3);				
 				}
 				break;
 			case 2:
 				fprintf(out, "%d,%d,", (int) data_out[i * 2], (int) data_out[i * 2 + 1]);
 				if(data_out[i * 2] != 0.0){
-					cvCircle(p_imgProcessedBlu,				//Draw a red circle around the detected object
-						 cvPoint(cvRound(data_out[i * 2]), cvRound(data_out[i * 2 + 1])),
-						 10,						//Circle of radius 10
-						 CV_RGB(255, 255, 255),		//White circle
-						 3);						//3 pixels thick
+					cvCircle(p_imgProcessedBlu,		
+						 cvPoint(cvRound(data_out[i * 2]),
+						 cvRound(data_out[i * 2 + 1])),
+						 10,				
+						 CV_RGB(255, 255, 255),	
+						 3);						
 				}
 				break;
 			case 3:
 				//Last beacon: include line break
 				fprintf(out, "%d,%d\n", (int) data_out[i * 2], (int) data_out[i * 2 + 1]);
 				if(data_out[i * 2] != 0.0){
-					cvCircle(p_imgProcessedYlo,				//Draw a red circle around the detected object
-						 cvPoint(cvRound(data_out[i * 2]), cvRound(data_out[i * 2 + 1])),
-						 10,						//Circle of radius 10
-						 CV_RGB(255, 255, 255),		//White circle
-						 3);						//3 pixels thick
+					cvCircle(p_imgProcessedYlo,	
+						 cvPoint(cvRound(data_out[i * 2]),
+						 cvRound(data_out[i * 2 + 1])),
+						 10,			
+						 CV_RGB(255, 255, 255),
+						 3);				
 				}
 				break;
 			default:
 				fprintf(stderr, "Somehow the switch statement got broken. Look into it.\n");
+				fclose(out);
+				free(outputFileName);
+				free(processedImages);
+				free(data_out);
+				cvReleaseCapture(&p_capVideo);
 				return -1;
 				break;
 			}//End beacon switch statement
@@ -299,23 +350,33 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 
 		//cvShowImage("Original", p_imgOriginal);	//Original image, with circle overlaid
 
+		//Combine filtered images, with overlaid circles, into one visible image
 		cvAddWeighted(p_imgProcessedBlu, 1, p_imgProcessedRed, 1, 0.0, p_imgProcessed);
 		cvAddWeighted(p_imgProcessed, 1, p_imgProcessedGrn, 1, 0.0, p_imgProcessed);
 		cvAddWeighted(p_imgProcessed, 1, p_imgProcessedYlo, 1, 0.0, p_imgProcessed);
 
+		//...and hope nothing goes wrong!
 		if(!p_imgProcessed){
 			fprintf(stderr, "ERROR: p_imgProcessed still NULL!\n");
+			free(outputFileName);
+			fclose(out);
+			free(processedImages);
+			free(data_out);
+			cvReleaseCapture(&p_capVideo);
 			return -1;
 		}
 
-		cvShowImage("Processed", p_imgProcessed);	//Image after processing
+		//Show off your work
+		cvShowImage("Processed", p_imgProcessed);
 
-		cvReleaseMemStorage(&p_strStorage);		//Deallocate necessary storage variable to pass into cvHoughCircles
+		//Deallocate necessary storage variable
+		cvReleaseMemStorage(&p_strStorage);	
 
+		//If Esc key (ASCII 27) was pressed, jump out of while loop
 		charCheckForEscKey = cvWaitKey(10);
-		if(charCheckForEscKey == 27) break;		//If Esc key (ASCII 27) was pressed, jump out of while loop
+		if(charCheckForEscKey == 27) break;		
 
-		//Reset data array, allowing us to keep track of poor entries
+		//Reset buffer array
 		for(i = 0; i < NUM_BEACONS * 2; i++){
 			data_out[i] = 0.0;
 		}
@@ -327,6 +388,7 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 
 	}//End while
 
+	//Housekeeping...
 	cvReleaseCapture(&p_capVideo);
 
 	fclose(out);
@@ -335,7 +397,7 @@ int Tracker::AnalyzeVideo(const char *videoFileName, const char *textFileName)
 	free(data_out);
 	free(outputFileName);
 
-	cvDestroyWindow("Original");
+	//cvDestroyWindow("Original");
 	cvDestroyWindow("Processed");
 
 	return 0;
